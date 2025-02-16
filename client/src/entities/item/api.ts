55d31caa5694ec2454337115d45eadf6f332132c
Ivan Petrukhin <petruekhin@yandex.ru>
2025-02-16 23:49:03 +0300

    Fix error with too deep type instantiation

--- client/src/entities/item/api.ts
+++ client/src/entities/item/api.ts
@@ -1,22 +1,26 @@
 import { api } from '@/shared/api'
 import { type InputItem, type Item } from './model'
 
+// fix weird bug
+type CopiedInputItem = { [Key in keyof InputItem]: InputItem[Key] }
+type CopiedItem = { [Key in keyof Item]: Item[Key] }
+
 export const itemsApi = api.enhanceEndpoints({ addTagTypes: ['Item'] }).injectEndpoints({
   endpoints: (build) => ({
-    getItems: build.query<Item[], void>({ query: () => 'items', providesTags: ['Item'] }),
-    getItemById: build.query({
+    getItems: build.query<CopiedItem[], void>({ query: () => 'items', providesTags: ['Item'] }),
+    getItemById: build.query<CopiedItem, number>({
       query: (id: number) => `items/${id}`,
       providesTags: (_result, _error, arg) => [{ type: 'Item', id: arg }],
     }),
-    deleteItemById: build.mutation({
+    deleteItemById: build.mutation<void, number>({
       query: (id: number) => ({ url: `items/${id}`, method: 'DELETE' }),
       invalidatesTags: (_result, _error, arg) => [{ type: 'Item', id: arg }],
     }),
-    putItem: build.mutation({
+    putItem: build.mutation<CopiedItem, CopiedItem>({
       query: (item: Item) => ({ url: `items/${item.id}`, body: item, method: 'PUT' }),
       invalidatesTags: (_result, _error, arg) => [{ type: 'Item', id: arg.id }],
     }),
-    postItem: build.mutation({
+    postItem: build.mutation<CopiedItem, CopiedInputItem>({
       query: (item: InputItem) => ({ url: `items`, body: item, method: 'POST' }),
       invalidatesTags: ['Item'],
     }),
